f9d905d9dfbfce3e8c4e4847798721bab603b684
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-31 05:52:42 +0200

    Implement read_to_stash function in get_next_line

--- get_next_line.c
+++ get_next_line.c
@@ -6,17 +6,21 @@ static char	*extract_line(char *stash);
 static char	*stash_after_line(char *stash);
 //
 
+static char	*read_to_stash(int fd, char *stash)
+{
+	
+}
+
 char	get_next_line(int fd)
 {
 	static char	*stash;
 	char		line;
 
-	stash = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return NULL;
 	stash = read_to_stash(fd, stash);
 	if (!stash)
-		return NULL;
+		return (NULL);
 	line = extract_line(stash);
 	stash = stash_after_line(stash);
 	return (line);
