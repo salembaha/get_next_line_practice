0820cf144dab1c4a28e751cd97bfc0c9029bc8b5
Salim Bahassou <sbahasso@student.codam.nl>
2025-08-31 05:42:24 +0200

    Add get_next_line function implementation
    Implement get_next_line function to read a line from a file descriptor.

--- get_next_line.c
+++ get_next_line.c
@@ -5,11 +5,19 @@ static char	*read_to_stash(int fd, char *stash);
 static char	*extract_line(char *stash);
 static char	*stash_after_line(char *stash);
 //
+
 char	get_next_line(int fd)
 {
-	if (!fd)
-	{
-		
-	}
-	return (0);
+	static char	*stash;
+	char		line;
+
+	stash = NULL;
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return NULL;
+	stash = read_to_stash(fd, stash);
+	if (!stash)
+		return NULL;
+	line = extract_line(stash);
+	stash = stash_after_line(stash);
+	return (line);
 }
