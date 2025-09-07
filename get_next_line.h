30ffd069a8ba98708b83919b958e4e84a932d57b
Salim Bahassou <sbahasso@student.codam.nl>
2025-09-07 08:41:58 +0200

    Update get_next_line.h header guard and includes

--- get_next_line.h
+++ get_next_line.h
@@ -1,13 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sabahass <sabahass@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/07 08:06:31 by sabahass          #+#    #+#             */
+/*   Updated: 2025/09/07 08:06:49 by sabahass         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef GET_NEXT_LINE_H
-#define GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 char	*get_next_line(int fd);
-static char	*read_to_stash(int fd, char *stash);
-static char	*extract_line(char *stash);
-static char	*stash_after_line(char *stash);
 
 #endif
