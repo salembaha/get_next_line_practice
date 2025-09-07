956ba90ac4469c97a3735bc077e61a77d15dd04d
Salim Bahassou <sbahasso@student.codam.nl>
2025-09-07 09:14:07 +0200

    Update header comments and function declarations

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sabahass <sabahass@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 08:06:31 by sabahass          #+#    #+#             */
-/*   Updated: 2025/09/07 08:06:49 by sabahass         ###   ########.fr       */
+/*   Updated: 2025/09/07 09:13:52 by sabahass         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,5 +21,9 @@
 # endif
 
 char	*get_next_line(int fd);
+size_t	ft_strlen_gnl(const char *s);
+char	*ft_strchr_gnl(const char *s, int c);
+char	*ft_strjoin_gnl(char *s1, const char *s2);
+char	*ft_substr_gnl(const char *s, unsigned int start, size_t len);
 
 #endif
